69ae55f51dd5d70390930573e3b5e3d5e5d21ec8
Marek Žavoronok <marek.zavoronok@gmail.com>
2020-04-27 09:54:14 +0300

    Add login form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,6 +17,9 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if ($this->getUser()) {
+          return $this->redirectToRoute('app_feed');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
