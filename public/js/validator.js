977d38b3e547cdcba2e7acb0ab7c4eac19185db5
Marek Žavoronok <marek.zavoronok@gmail.com>
2020-04-27 22:51:40 +0300

    Beatify js

--- public/js/validator.js
+++ public/js/validator.js
@@ -1,33 +1,36 @@
 jQuery(document).ready(function() {
-	var $ = jQuery;
-	$("#registration_form_email").bind("change", validateFields);
+    var $ = jQuery;
+    $("#registration_form_email").bind("change", validateFields);
     $('#form-submit').on('click', function(e) {
-    	e.preventDefault();
+        e.preventDefault();
         validateFields('submit');
     })
-	function validateFields(type='') {
-		var email = $("#registration_form_email").val();
-	    $.post('/validate', {'email': email}, function(data) {
-	      var globalErrors = 0;
-	      for (var [key, value] of Object.entries(data)) {
-	    	if (Object.values(value).length > 0) {
-    	      for (var [propertyKey, properyErrors] of Object.entries(value)) {
-    	      	  var errors = '';
-		    	  properyErrors.forEach(val => {
-		    	    errors += '<li>' + val + '</li>';
-		    	    globalErrors += 1;
-		    	  });
-		    	  $('#email-group > small.text-danger').html('<ul>'+errors+'</ul>');
-		          $('#email-group > .label').addClass('text-danger');
-		          $('#email-group > .form-control').addClass('is-invalid');
-    	      }
-	       } else {
-             $('#email-group > small.text-danger').html('');
-             $('#email-group > .label').removeClass('text-danger');
-             $('#email-group > .form-control').removeClass('is-invalid');
-	       }
-	     }
-	     if (globalErrors === 0 && type === 'submit') $('#registration-form').submit();
-	   });
-	}
+
+    function validateFields(type = '') {
+        var email = $("#registration_form_email").val();
+        $.post('/validate', {
+            'email': email
+        }, function(data) {
+            var globalErrors = 0;
+            for (var [key, value] of Object.entries(data)) {
+                if (Object.values(value).length > 0) {
+                    for (var [propertyKey, properyErrors] of Object.entries(value)) {
+                        var errors = '';
+                        properyErrors.forEach(val => {
+                            errors += '<li>' + val + '</li>';
+                            globalErrors += 1;
+                        });
+                        $('#email-group > small.text-danger').html('<ul>' + errors + '</ul>');
+                        $('#email-group > .label').addClass('text-danger');
+                        $('#email-group > .form-control').addClass('is-invalid');
+                    }
+                } else {
+                    $('#email-group > small.text-danger').html('');
+                    $('#email-group > .label').removeClass('text-danger');
+                    $('#email-group > .form-control').removeClass('is-invalid');
+                }
+            }
+            if (globalErrors === 0 && type === 'submit') $('#registration-form').submit();
+        });
+    }
 })
