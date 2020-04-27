fa3e3a1538d70ccf9b53623c4dc3e3aa2e7ae622
Marek Žavoronok <marek.zavoronok@gmail.com>
2020-04-27 17:48:35 +0300

    Add most frequent words

--- src/Feeds/FeedCreator.php
+++ src/Feeds/FeedCreator.php
@@ -14,12 +14,10 @@ class FeedCreator
 	 * Create feed from thirt party resource
 	 * @return array
 	 */
-
 	public function create():array
 	{
         $xml = simplexml_load_string(file_get_contents($this->url));
-        $output = [];
-        dump($xml);
+        $output = ['feed'];
         foreach ($xml->entry as $entry) {
         	$modifiedEntry = [];
         	$modifiedEntry['updated'] = date('Y-m-d H:i:s', strtotime($entry->updated));
@@ -27,8 +25,36 @@ class FeedCreator
         	$modifiedEntry['title'] = $entry->title;
         	$modifiedEntry['summary'] = $entry->summary;
         	$modifiedEntry['link'] = $entry->link['href'];
-        	$output[] = $modifiedEntry;
+        	$output['feed'][] = $modifiedEntry;
         }
+        $this->determineMostFrequentWords($output);
         return $output;
+	}
+
+	/**
+	 * Calculate most frequently workds in feed
+	 * @param $entries - array
+	 * @return array
+     */
+	private function determineMostFrequentWords(array &$entries):void
+	{
+		$words = [];
+        foreach ($entries['feed'] as $entry) {
+        	foreach ($entry as $attribute) {
+                $splited = explode(' ', $attribute);
+                if (!empty($splited)) {
+	                foreach ($splited as $spl) {
+	                	if (!ctype_alpha($spl)) continue;
+	                    if (!isset($words[$spl])) {
+                            $words[$spl] = 0;
+	                    }
+	                    $words[$spl]++;
+	                }
+                }
+        	}
+        }
+        uasort($words, fn($a, $b) => $b - $a);
+        $words = array_splice($words, 0, 10);
+        $entries['words'] = $words;
 	}
 }
