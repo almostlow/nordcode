598aba8b1c6a5c651c5d7f943322a1b9398571ad
Marek Žavoronok <marek.zavoronok@gmail.com>
2020-04-27 22:48:06 +0300

    Add cache warmer

---
+++ src/Cache/CacheWarmer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Cache;
+
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+use App\Feeds\FeedCreator;
+
+class CacheWarmer implements CacheWarmerInterface
+{
+	private $feedCreator;
+
+	public function __construct(FeedCreator $feedCreator)
+	{
+		$this->feedCreator = $feedCreator;
+	}
+
+    /**
+     * Load cache before user hit web
+     * @param string $cacheDirectory
+     */
+    public function warmUp($cacheDirectory)
+    {
+        $this->feedCreator->create();
+    }
+
+    public function isOptional()
+    {
+        return true;
+    }
+}
